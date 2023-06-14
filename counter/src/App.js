ab26d3a4b383d61e277d43f03145a1236299b909
Santonu-Naskar <95866317+Santonu-Naskar@users.noreply.github.com>
2023-06-14 15:33:31 +0530

    Add files via upload

---
+++ counter/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import Counter from './components/Counter';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Counter App with Redux !</h1>
+        </header>
+        <Counter/>
+      </div>
+    );
+  }
+}
+
+export default App;
